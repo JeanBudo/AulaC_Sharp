6986ce77b7206577d6fbe7fd8104dff2d1f70824
rodrigofricate <rodrigofricate@yahoo.com.br>
2023-04-26 09:33:09 -0300

    Rebuild

---
+++ AulaPratica/AulaPratica/Aula03/Entities/Objetos/CdPlayStation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPratica.Aula03.Entities
+{
+    public class CdPlayStation : Preco
+    {
+        public string objectName = "Desconhecido";
+        string gameName = "desconhecido";
+        string description = "Você ve um CD do jogo ";
+
+       public CdPlayStation(string nameOfGame, double price, double percentageDiscount)
+        {
+            this.gameName = nameOfGame;
+            this.price = price;
+            this.percentageDiscount = percentageDiscount;
+        }
+
+        public override void Description()
+        {
+            Console.WriteLine(description + " " + gameName + " custando: " + GetPriceAsString());
+        }
+        public void Hello() { }
+
+
+
+    }
+}
---
+++ AulaPratica/AulaPratica/Aula03/Entities/Objetos/SofaVelho.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPratica.Aula03.Entities
+{
+    public class SofaVelho : Preco
+    {
+        public string objectName = "Sofá velho";
+        string objectMaterial = "courino e madeira";
+        string objectDescription = "um sofá velho de dois lugares";
+        public SofaVelho(double price, double percentageDiscount) {
+            this.price = price;
+            this.percentageDiscount = percentageDiscount;
+        }
+
+        public override void Description()
+        {
+            Console.WriteLine("Você ve uma " + objectName + " feita de " + objectMaterial + " " + objectDescription + " custando: " + GetPriceAsString());
+        }
+        public void Hello() { }
+
+    }
+}
